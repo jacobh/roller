4daacb78b37af41b86c1aa06169fffa7f8c7fa4d
Jacob Haslehurst <jacob@haslehurst.net>
2020-01-14 00:27:42 +1100

    start trying to send red/blue test pattern

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,40 @@
+use ola::ola_server_service_client::OlaServerServiceClient;
+use ola::DmxData;
+
 pub mod ola {
     tonic::include_proto!("ola.proto");
 }
 
-fn main() {
-    println!("Hello, world!");
+fn pad_512(mut vec: Vec<u8>) -> Vec<u8> {
+    while vec.len() < 512 {
+        vec.push(0)
+    }
+    vec
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = OlaServerServiceClient::connect("http://127.0.0.1:9010").await?;
+
+    loop {
+        println!("red");
+        client.stream_dmx_data(DmxData {
+            universe: 10,
+            data: pad_512(vec![255, 0, 0, 0]),
+            priority: Some(1),
+        }).await?;
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        println!("blue");
+        client.stream_dmx_data(DmxData {
+            universe: 10,
+            data: pad_512(vec![0, 255, 0, 0]),
+            priority: Some(1),
+        }).await?;
+
+        std::thread::sleep(std::time::Duration::from_secs(1));
+    }
+
+    Ok(())
 }
