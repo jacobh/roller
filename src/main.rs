c2e5b54300633629d91a5eb60ff2be32b6e89aa9
Jacob Haslehurst <jacob@haslehurst.net>
2020-01-24 22:45:50 +1100

    start implementing ola client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use async_std::net::{TcpStream, ToSocketAddrs};
+
 fn pad_512(mut vec: Vec<u8>) -> Vec<u8> {
     while vec.len() < 512 {
         vec.push(0)
@@ -7,6 +9,20 @@ fn pad_512(mut vec: Vec<u8>) -> Vec<u8> {
 
 pub mod ola {
     include!(concat!(env!("OUT_DIR"), "/ola.proto.rs"));
+}
+
+struct OlaClient {
+    stream: TcpStream,
+}
+impl OlaClient {
+    async fn connect(host: impl ToSocketAddrs) -> Result<OlaClient, async_std::io::Error> {
+        Ok(OlaClient {
+            stream: TcpStream::connect(host).await?,
+        })
+    }
+    async fn connect_localhost() -> Result<OlaClient, async_std::io::Error> {
+        OlaClient::connect("localhost:9010").await
+    }
 }
 
 fn main() {
