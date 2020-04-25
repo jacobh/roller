732afd839dce9312af929078d15f7073914e6604
Jacob Haslehurst <jacob@haslehurst.net>
2020-04-25 15:31:59 +1000

    tidy

--- web_ui/main.js
+++ web_ui/main.js
@@ -1,6 +1,6 @@
-import init, { run_app } from './pkg/yew_wasm_pack_minimal.js';
+import init, { run_app } from './pkg/roller_web_ui.js';
 async function main() {
-   await init('/pkg/yew_wasm_pack_minimal_bg.wasm');
+   await init('/pkg/roller_web_ui_bg.wasm');
    run_app();
 }
 main()
