a2bd9cf04f5fa22ccd206254f24000bbc81d28d1
Jacob Haslehurst <jacob@haslehurst.net>
2020-05-01 23:36:42 +1000

    coord to note conversion

--- roller/src/web.rs
+++ roller/src/web.rs
@@ -1,5 +1,17 @@
 use warp::Filter;
 
+use midi::Note;
+use roller_protocol::{ButtonCoordinate, ButtonGridLocation};
+
+// Specific for akai apc mini. really the internal button location should be specific with coords
+fn coordinate_to_note(loc: &ButtonGridLocation, coord: &ButtonCoordinate) -> Note {
+    match loc {
+        ButtonGridLocation::Main => Note::new((8 * coord.row_idx + coord.column_idx) as u8),
+        ButtonGridLocation::MetaRight => Note::new((89 - coord.row_idx) as u8),
+        ButtonGridLocation::MetaBottom => Note::new((64 + coord.column_idx) as u8),
+    }
+}
+
 pub fn serve_frontend() {
     let index = warp::get().and(warp::fs::file("web_ui/index.html"));
     let assets = warp::get().and(warp::fs::dir("web_ui"));
