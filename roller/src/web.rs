5250cf1c348cac3d1661a47d23098bb193f727b8
Jacob Haslehurst <jacob@haslehurst.net>
2020-05-02 00:16:37 +1000

    hook up websocket handling

--- roller/src/web.rs
+++ roller/src/web.rs
@@ -1,7 +1,11 @@
-use warp::Filter;
+use futures::prelude::*;
+use warp::{
+    ws::{WebSocket, Ws},
+    Filter,
+};
 
 use midi::Note;
-use roller_protocol::{ButtonCoordinate, ButtonGridLocation};
+use roller_protocol::{ButtonCoordinate, ButtonGridLocation, ClientMessage};
 
 use crate::ControlEvent;
 
@@ -14,11 +18,57 @@ fn coordinate_to_note(loc: &ButtonGridLocation, coord: &ButtonCoordinate) -> Not
     }
 }
 
-pub fn serve_frontend(_event_sender: async_std::sync::Sender<ControlEvent>) {
+async fn browser_session(
+    websocket: WebSocket,
+    event_sender: async_std::sync::Sender<ControlEvent>,
+) {
+    let (_tx, mut rx) = websocket.split();
+
+    while let Some(message) = rx.next().await {
+        match message {
+            Err(e) => {
+                println!("error reading from client: {:?}", e);
+                return;
+            }
+            Ok(msg) => {
+                if !msg.is_binary() {
+                    continue;
+                }
+
+                let msg = bincode::deserialize::<ClientMessage>(msg.as_bytes())
+                    .expect("bincode::deserialize");
+
+                println!("{:?}", msg);
+
+                let control_event = match msg {
+                    ClientMessage::ButtonPressed(loc, coord) => {
+                        let _note = coordinate_to_note(&loc, &coord);
+                        // TODO we need the button mapping here to determine which button was pressed
+                        // Placeholder
+                        ControlEvent::TapTempo(std::time::Instant::now())
+                    }
+                };
+                event_sender.send(control_event).await;
+            }
+        }
+    }
+}
+
+pub fn serve_frontend(event_sender: async_std::sync::Sender<ControlEvent>) {
     let index = warp::get().and(warp::fs::file("web_ui/index.html"));
     let assets = warp::get().and(warp::fs::dir("web_ui"));
 
-    let app = warp::path::end().and(index).or(assets);
+    let websocket = warp::get()
+        .and(warp::path("ws"))
+        .and(warp::ws())
+        .and(warp::any().map(move || event_sender.clone()))
+        .map(
+            move |ws: Ws, sender: async_std::sync::Sender<ControlEvent>| {
+                ws.on_upgrade(move |websocket| browser_session(websocket, sender))
+            },
+        );
+
+    let app = warp::path::end().and(index).or(websocket).or(assets);
 
     let mut rt = tokio::runtime::Runtime::new().unwrap();
 
--- roller_protocol/src/lib.rs
+++ roller_protocol/src/lib.rs
@@ -1,5 +1,5 @@
+use serde::{Deserialize, Serialize};
 use std::fmt;
-use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub enum Message {
