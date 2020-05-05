7ab59a3a383dc944ae9f93636634d34c8ee00079
Jacob Haslehurst <jacob@haslehurst.net>
2020-05-05 21:13:05 +1000

    hook up config arg

--- roller/src/main.rs
+++ roller/src/main.rs
@@ -1,7 +1,9 @@
 use async_std::sync::Arc;
 use futures::pin_mut;
 use futures::stream::{self, StreamExt};
+use std::path::PathBuf;
 use std::time::{Duration, Instant};
+use structopt::StructOpt;
 
 mod clock;
 mod color;
@@ -19,6 +21,18 @@ use crate::lighting_engine::{ControlEvent, EngineState};
 
 #[global_allocator]
 static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "roller")]
+struct CliArgs {
+    #[structopt(
+        short,
+        long,
+        default_value = "roller_project.toml",
+        parse(from_os_str)
+    )]
+    config: PathBuf,
+}
 
 async fn run_tick<'a>(
     state: &mut EngineState<'a>,
@@ -70,7 +84,9 @@ async fn run_tick<'a>(
 
 #[async_std::main]
 async fn main() -> Result<(), async_std::io::Error> {
-    let project = project::Project::load("./roller_project.toml").await?;
+    let args = CliArgs::from_args();
+    
+    let project = project::Project::load(args.config).await?;
     let mut fixtures = project.fixtures().await?;
 
     let midi_mapping = Arc::new(control::default_midi_mapping());
