89d3214c08c8347992b9b0462063387c0f40d137
Jacob Haslehurst <jacob@haslehurst.net>
2020-07-11 21:54:26 +1000

    lay out preview cells

--- web_ui/src/pages/preview.rs
+++ web_ui/src/pages/preview.rs
@@ -1,4 +1,5 @@
 use im_rc::HashMap;
+use itertools::Itertools;
 use yew::prelude::*;
 
 use roller_protocol::fixture::{FixtureId, FixtureParams, FixtureState};
@@ -13,31 +14,40 @@ pub struct PurePreviewPage {
 }
 impl PureComponent for PurePreviewPage {
     fn render(&self) -> Html {
-        let fixtures: Vec<Html> = self
+        let fixtures: Vec<(&FixtureId, &FixtureParams, &FixtureState)> = self
             .fixture_states
             .iter()
             .filter_map(|(fixture_id, (params, state))| match state {
                 Some(state) => Some((fixture_id, params, state)),
                 None => None,
             })
-            .map(|(_fixture_id, params, state)| {
-                html! {
-                    <dl>
-                        <dt>{"Profile"}</dt>
-                        <dd>{params.profile.label.clone()}</dd>
-                        <dt>{"Location"}</dt>
-                        <dd>{format!("{:?}", params.location)}</dd>
-                        <dt>{"Dimmer"}</dt>
-                        <dd>{format!("{:?}", state.dimmer)}</dd>
-                    </dl>
-                }
-            })
             .collect();
+
+        let rows = fixtures
+            .iter()
+            .filter_map(|(_, params, _)| params.location.as_ref())
+            .map(|location| location.y)
+            .unique()
+            .count();
+        let columns = fixtures
+            .iter()
+            .filter_map(|(_, params, _)| params.location.as_ref())
+            .map(|location| location.x)
+            .unique()
+            .count();
 
         html! {
             <div class="page-contents">
                 <h2>{"Fixtures"}</h2>
-                {fixtures}
+                <div>
+                    {(0..rows).map(|row_idx| html! {
+                        <div class="preview__row">
+                        {(0..columns).map(|column_idx| html! {
+                            <div id={format!("preview__cell-{}-{}", column_idx, row_idx)} class="preview__cell"></div>
+                        }).collect::<Html>()}
+                        </div>
+                    }).collect::<Html>()}
+                </div>
             </div>
         }
     }
