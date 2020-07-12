c2934727e5f091c2c4dd8a47ddabecf1c497bdf5
Jacob Haslehurst <jacob@haslehurst.net>
2020-07-12 19:29:54 +1000

    remove FxIndexMap, not needed

--- roller_protocol/src/utils.rs
+++ roller_protocol/src/utils.rs
@@ -1,9 +1,3 @@
-use indexmap::IndexMap;
-use rustc_hash::FxHasher;
-use std::hash::BuildHasherDefault;
-
-pub type FxIndexMap<K, V> = IndexMap<K, V, BuildHasherDefault<FxHasher>>;
-
 pub fn clamp<T>(x: T, min: T, max: T) -> T
 where
     T: PartialOrd,
