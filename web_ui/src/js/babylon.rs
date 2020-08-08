4761c20119f6a2f14f588395809d841f843a1be6
Jacob Haslehurst <jacob@haslehurst.net>
2020-08-09 01:35:38 +1000

    move namespace declaration to top of extern

--- web_ui/src/js/babylon.rs
+++ web_ui/src/js/babylon.rs
@@ -1,18 +1,18 @@
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
+#[wasm_bindgen(js_namespace = BABYLON)]
 extern "C" {
     pub type Engine;
 
-    #[wasm_bindgen(constructor, js_namespace = BABYLON)]
+    #[wasm_bindgen(constructor)]
     pub fn new(canvas_element: web_sys::HtmlCanvasElement, x: bool) -> Engine;
 
     // static get Version(): string;
-    #[wasm_bindgen(static_method_of = Engine, getter, js_namespace = BABYLON, js_name = "Version")]
+    #[wasm_bindgen(static_method_of = Engine, getter, js_name = "Version")]
     pub fn version() -> String;
 
     pub type Scene;
 
-    #[wasm_bindgen(constructor, js_namespace = BABYLON)]
+    #[wasm_bindgen(constructor)]
     pub fn new(engine: &Engine) -> Scene;
 }
