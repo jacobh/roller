4be3552b18bd684fa202eedc1934851c3f1289d4
Jacob Haslehurst <jacob@haslehurst.net>
2020-08-09 01:25:29 +1000

    print babylon engine version to console

--- web_ui/src/pages/preview_3d.rs
+++ web_ui/src/pages/preview_3d.rs
@@ -1,23 +1,48 @@
 use im_rc::HashMap;
-use itertools::Itertools;
 use yew::prelude::*;
 
 use roller_protocol::fixture::{FixtureId, FixtureParams, FixtureState};
 
-use crate::pure::{Pure, PureComponent};
-
-pub type Preview3dPage = Pure<PurePreview3dPage>;
+use crate::{console_log, js::babylon, yewtil::neq_assign::NeqAssign};
 
 #[derive(Properties, Clone, PartialEq)]
-pub struct PurePreview3dPage {
+pub struct PurePreview3dProps {
     pub fixture_states: HashMap<FixtureId, (FixtureParams, Option<FixtureState>)>,
 }
-impl PureComponent for PurePreview3dPage {
-    fn render(&self) -> Html {
+
+pub struct Preview3dPage {
+    props: PurePreview3dProps,
+}
+
+impl Component for Preview3dPage {
+    type Message = ();
+    type Properties = PurePreview3dProps;
+
+    fn create(props: Self::Properties, _link: ComponentLink<Self>) -> Self {
+        Preview3dPage { props }
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, props: Self::Properties) -> ShouldRender {
+        self.props.neq_assign(props)
+    }
+
+    fn rendered(&mut self, first_render: bool) {
+        if first_render {
+            console_log!("{}", babylon::Engine::version())
+        }
+    }
+
+    fn view(&self) -> Html {
         html! {
             <div class="page-contents">
                 <h2>{"Fixtures 3D"}</h2>
-                <div></div>
+                <div>
+                    <canvas id="preview-3d-canvas"></canvas>
+                </div>
             </div>
         }
     }
