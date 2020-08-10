437c827a8adfe2ed0dc605f5f466f22c1bbf1a5d
Jacob Haslehurst <jacob@haslehurst.net>
2020-08-10 19:06:01 +1000

    remove unneeded unsafe

--- web_ui/src/utils.rs
+++ web_ui/src/utils.rs
@@ -5,7 +5,7 @@ use yew::prelude::*;
 macro_rules! console_log {
     // Note that this is using the `log` function imported above during
     // `bare_bones`
-    ($($t:tt)*) => (unsafe { web_sys::console::log_1(&wasm_bindgen::JsValue::from_str(&format_args!($($t)*).to_string())) })
+    ($($t:tt)*) => (web_sys::console::log_1(&wasm_bindgen::JsValue::from_str(&format_args!($($t)*).to_string())))
 }
 
 pub fn callback_fn<T, F: Fn(T) + Sized + 'static>(f: F) -> Callback<T> {
