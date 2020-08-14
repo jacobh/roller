b4383757d3b090e4768858ea8f1d6078e46399de
Jacob Haslehurst <jacob@haslehurst.net>
2020-08-14 23:47:53 +1000

    compute base length for given beam angle

--- web_ui/src/pages/preview_3d/light.rs
+++ web_ui/src/pages/preview_3d/light.rs
@@ -6,12 +6,16 @@ pub struct CreateLightArgs<'a> {
     pub origin_position: Vector,
 }
 pub fn create_light<'a>(args: CreateLightArgs<'a>) {
+    let beam_angle = f64::to_radians(30.0);
+    let cone_length = 50.0;
+    let base_length = f64::tan(beam_angle / 2.0) * cone_length * 2.0;
+
     let cone = babylon::MeshBuilder::create_cylinder(
         "light_cone".to_string(),
         babylon::CreateCylinderOptions {
-            height: Some(30.0),
+            height: Some(cone_length),
             diameterTop: Some(0.5),
-            diameterBottom: Some(10.0),
+            diameterBottom: Some(base_length),
             tessellation: Some(96.0),
             subdivisions: Some(4.0),
             enclose: Some(false),
