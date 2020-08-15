c7af27bad597d5e7eb432d82c0f9cb9ab7f168ab
Jacob Haslehurst <jacob@haslehurst.net>
2020-08-15 16:54:45 +1000

    create Light wrapper struct

--- web_ui/src/pages/preview_3d/light.rs
+++ web_ui/src/pages/preview_3d/light.rs
@@ -1,16 +1,22 @@
 use crate::{js::babylon, pages::preview_3d::Vector};
 
+#[derive(Debug)]
+pub struct Light {
+    cone_mesh: babylon::Mesh,
+    spot_light: babylon::SpotLight,
+}
+
 pub struct CreateLightArgs<'a> {
     pub scene: &'a babylon::Scene,
     pub lightbeam_falloff: &'a babylon::Material,
     pub origin_position: Vector,
 }
-pub fn create_light<'a>(args: CreateLightArgs<'a>) {
+pub fn create_light<'a>(args: CreateLightArgs<'a>) -> Light {
     let beam_angle = f64::to_radians(30.0);
     let cone_length = 50.0;
     let base_length = f64::tan(beam_angle / 2.0) * cone_length * 2.0;
 
-    let cone = babylon::MeshBuilder::create_cylinder(
+    let cone_mesh = babylon::MeshBuilder::create_cylinder(
         "light_cone".to_string(),
         babylon::CreateCylinderOptions {
             height: Some(cone_length),
@@ -24,8 +30,8 @@ pub fn create_light<'a>(args: CreateLightArgs<'a>) {
         },
         Some(&args.scene),
     );
-    cone.set_position(&babylon::Vector3::from(&args.origin_position));
-    cone.set_material(&args.lightbeam_falloff);
+    cone_mesh.set_position(&babylon::Vector3::from(&args.origin_position));
+    cone_mesh.set_material(&args.lightbeam_falloff);
 
     let spot_light = babylon::SpotLight::new(
         "spot_light".to_string(),
@@ -40,4 +46,9 @@ pub fn create_light<'a>(args: CreateLightArgs<'a>) {
         &args.scene,
     );
     spot_light.set_intensity(8.0);
+
+    Light {
+        spot_light,
+        cone_mesh,
+    }
 }
