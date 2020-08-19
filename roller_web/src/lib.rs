b9aacfc8eaea704c4853bf76f3dd4f2628902eac
Jacob Haslehurst <jacob@haslehurst.net>
2020-08-19 18:18:26 +1000

    hook up initial_fixture_group_states

--- roller_web/src/lib.rs
+++ roller_web/src/lib.rs
@@ -10,7 +10,8 @@ use warp::{
 
 use roller_protocol::{
     control::{ButtonCoordinate, ButtonGridLocation, ButtonState, InputEvent},
-    fixture::{FixtureId, FixtureParams},
+    fixture::{FixtureGroupId, FixtureId, FixtureParams},
+    lighting_engine::FixtureGroupState,
     ClientMessage, ServerMessage,
 };
 
@@ -18,6 +19,10 @@ async fn browser_session(
     websocket: WebSocket,
     fixture_params: FxHashMap<FixtureId, FixtureParams>,
     initial_button_states: FxHashMap<(ButtonGridLocation, ButtonCoordinate), (String, ButtonState)>,
+    initial_fixture_group_states: (
+        FixtureGroupState,
+        FxHashMap<FixtureGroupId, FixtureGroupState>,
+    ),
     server_message_recv: impl Stream<Item = ServerMessage> + Unpin,
     event_sender: Sender<InputEvent>,
 ) {
@@ -38,6 +43,18 @@ async fn browser_session(
                 .collect(),
         ),
         ServerMessage::FixtureParamsUpdated(fixture_params.into_iter().collect()),
+        ServerMessage::FixtureGroupStatesUpdated({
+            let (base_state, group_states) = initial_fixture_group_states;
+
+            vec![(None, base_state)]
+                .into_iter()
+                .chain(
+                    group_states
+                        .into_iter()
+                        .map(|(id, state)| (Some(id), state)),
+                )
+                .collect()
+        }),
     ];
 
     for message in initial_messages {
@@ -98,21 +115,36 @@ pub fn serve_frontend(
     event_sender: Sender<InputEvent>,
 ) {
     let initial_button_states = Arc::new(Mutex::new(initial_button_states));
+    let initial_fixture_group_states = Arc::new(Mutex::new((
+        FixtureGroupState::default(),
+        FxHashMap::default(),
+    )));
     let server_message_channel: BroadcastChannel<ServerMessage> = BroadcastChannel::new();
 
     // Update initial button states with incoming messages
     let initial_button_states2 = initial_button_states.clone();
+    let initial_fixture_group_states2 = initial_fixture_group_states.clone();
     let (mut server_message_sender, _) = server_message_channel.clone().split();
     async_std::task::spawn(async move {
         while let Some(server_message) = server_message_recv.next().await {
             match &server_message {
                 ServerMessage::ButtonStatesUpdated(coord_states) => {
                     let mut states = initial_button_states2.lock().await;
-                    for (loc, coord, state) in coord_states.iter() {
+                    for (loc, coord, state) in coord_states.clone() {
                         states
-                            .entry((loc.clone(), coord.clone()))
-                            .and_modify(|(_label, prev_state)| *prev_state = *state)
-                            .or_insert_with(|| (String::new(), *state));
+                            .entry((loc, coord))
+                            .and_modify(|(_label, prev_state)| *prev_state = state)
+                            .or_insert_with(|| (String::new(), state));
+                    }
+                }
+                ServerMessage::FixtureGroupStatesUpdated(updates) => {
+                    let mut states = initial_fixture_group_states2.lock().await;
+                    for (id, state) in updates.clone() {
+                        if let Some(id) = id {
+                            states.1.insert(id, state);
+                        } else {
+                            states.0 = state;
+                        }
                     }
                 }
                 _ => {}
@@ -132,6 +164,8 @@ pub fn serve_frontend(
             let event_sender = event_sender.clone();
             let initial_button_states =
                 async_std::task::block_on(initial_button_states.lock()).clone();
+            let initial_fixture_group_states =
+                async_std::task::block_on(initial_fixture_group_states.lock()).clone();
             let (_, server_message_recv) = server_message_channel.clone().split();
 
             ws.on_upgrade(move |websocket| {
@@ -139,6 +173,7 @@ pub fn serve_frontend(
                     websocket,
                     fixture_params,
                     initial_button_states,
+                    initial_fixture_group_states,
                     server_message_recv,
                     event_sender,
                 )
